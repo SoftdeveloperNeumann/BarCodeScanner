b5c498a19554584d4365ecedd819d25f72e704d5
Frank <f.neumann.berlin@googlemail.com>
2023-02-14 08:53:00 +0100

    Artikel als Result von DB erhalten

--- app/src/main/java/com/example/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/barcodescanner/MainActivity.kt
@@ -4,16 +4,24 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.barcodescanner.databinding.ActivityMainBinding
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +34,66 @@ class MainActivity : AppCompatActivity() {
 
             try {
                 activityResultLauncher.launch(intent)
-            }catch (e: ActivityNotFoundException){
+            } catch (e: ActivityNotFoundException) {
                 Toast.makeText(this, "Scanner nicht installiert", Toast.LENGTH_SHORT).show()
             }
         }
 
-        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            runBlocking {
-                if(it.resultCode == RESULT_OK && it.data !=null){
-                    binding.tvStatus.text = it.data!!.getStringExtra("SCAN_RESULT_FORMAT")
-                    binding.tvResult.text = it.data!!.getStringExtra("SCAN_RESULT")
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                runBlocking {
+                    if (it.resultCode == RESULT_OK && it.data != null) {
+                        binding.tvStatus.text = it.data!!.getStringExtra("SCAN_RESULT_FORMAT")
+
+                        val text = async {
+                            it.data!!.getStringExtra("SCAN_RESULT")
+                                ?.let { value -> getProduct(value) }
+                        }
+
+                        binding.tvResult.text = text.await()
+                    }
                 }
             }
+    }
+
+    private suspend fun getProduct(scanResult: String): String {
+        var result: String? = null
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            result = doInBackground(scanResult)
         }
+        job.join()
+        Log.d(TAG, "getProduct: $result")
+        val rootObject = JSONObject(result)
+        if(rootObject.has("product")){
+            val product = rootObject.getJSONObject("product")
+            if(product.has("product_name")){
+                return product.getString("product_name")
+            }
+        }
+        
+        return "Artikel nicht gefunden"
+    }
+
+    private fun doInBackground(scanResult: String): String {
+        val baseUrl = "https://world.openfoodfacts.org/api/v0/product/"
+        val requestUrl = "$baseUrl$scanResult.json"
+        var url: URL?= null
+        val result = java.lang.StringBuilder()
+        try {
+            url = URL(requestUrl)
+        }catch(e: MalformedURLException){
+            Log.e(TAG, "Url konnte nicht erstellt werden ", e )
+        }
+        try{
+            BufferedReader(InputStreamReader(url?.openConnection()?.getInputStream())).use { 
+                var line:String?
+                while (it.readLine().also { line = it }!=null){
+                    result.append(line)
+                }
+            }
+        }catch(e:IOException){
+            Log.e(TAG, "Fehler beim lesen der Daten ",e )
+        }
+        return result.toString()
     }
 }
